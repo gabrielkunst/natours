1e650a93980ed8efc76965619c4cc978cf37a1ad
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-11-02 13:25:05 -0300

    feat: added post /api/v1/tours route handle

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+app.use(express.json());
 
 const toursAsJson = fs.readFileSync(
   `${__dirname}/dev-data/data/tours-simple.json`,
@@ -18,6 +19,29 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.at(-1).id + 1;
+
+  const newTour = {
+    id: newId,
+    ...req.body,
+  };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const PORT = 5000;
