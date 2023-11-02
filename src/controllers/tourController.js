d433f8027add1e0ec8545cf39aa6ad30b14a1843
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-11-02 18:39:02 -0300

    feat: adding validateId middleware

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -18,16 +18,7 @@ function getAllTours(req, res) {
 }
 
 function createTour(req, res) {
-  const newTourFromBody = req.body;
-
-  if (!newTourFromBody || Object.keys(newTourFromBody).length === 0) {
-    res.status(400).json({
-      status: "fail",
-      message: "Missing tour object",
-    });
-    return;
-  }
-
+  const newTourFromBody = req.body; // validated on middleware
   const newTourId = Math.floor(Math.random() * 1000000);
   const newTour = {
     id: newTourId,
@@ -54,15 +45,7 @@ function createTour(req, res) {
 
 function getTourById(req, res) {
   try {
-    const id = Number(req.params.id);
-    if (!id || isNaN(id)) {
-      res.status(400).json({
-        status: "fail",
-        message: "Invalid id",
-      });
-      return;
-    }
-
+    const id = req.params.id; // validated on middleware
     const tour = tours.find((tour) => tour.id === id);
     if (!tour) {
       res.status(404).json({
@@ -88,15 +71,7 @@ function getTourById(req, res) {
 
 function deleteTourById(req, res) {
   try {
-    const id = Number(req.params.id);
-    if (!id || isNaN(id)) {
-      res.status(400).json({
-        status: "fail",
-        message: "Invalid id",
-      });
-      return;
-    }
-
+    const id = req.params.id; // validated on middleware
     const tour = tours.find((tour) => tour.id === id);
     if (!tour) {
       res.status(400).json({
@@ -117,9 +92,38 @@ function deleteTourById(req, res) {
   }
 }
 
+function validateId(req, res, next, value, name) {
+  const id = Number(value);
+  if (!id || isNaN(id)) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid id",
+    });
+    return;
+  }
+  req.params[name] = id;
+  next();
+}
+
+function validateRequestBody(req, res, next) {
+  const tourFromBody = req.body;
+
+  if (!tourFromBody.name || !tourFromBody.price) {
+    res.status(400).json({
+      status: "fail",
+      message: "Missing name or price",
+    });
+    return;
+  }
+
+  next();
+}
+
 module.exports = {
   getAllTours,
   createTour,
   getTourById,
   deleteTourById,
+  validateId,
+  validateRequestBody,
 };
--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const tourController = require("../controllers/tourController");
 const router = express.Router();
 
+router.param("id", tourController.validateId);
+
 router
   .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.validateRequestBody, tourController.createTour);
 router
   .route("/:id")
   .get(tourController.getTourById)
