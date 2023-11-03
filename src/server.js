25557951e116da220fa8145cdffc8e6e386f6a93
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-11-02 22:09:11 -0300

    feat: adding mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: `${__dirname}/../config.env` });
 
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then((connection) => {
+  console.log("Database connected");
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
